b19aeb348f6041bf830e911e0aaeef91836a8633
Norbert de Langen <ndelangen@me.com>
2018-02-09 13:20:23 +0100

    Voor ruben

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import data from './data.json';
+
+const d3 = window.d3;
+
+const myData = data.filter(d => !d.actor_login.includes('bot')).map(e => ({
+  user: e.actor_login,
+  type: e.type,
+  date: new Date(e.created_at),
+  metadata: e.payload,
+}));
+
+const myScale = d3.scaleLinear().domain([0, 10])
+const typeScale = d3.scaleOrdinal(d3.schemeAccent).domain(['PushEvent', 'IssueCommentEvent', 'DeleteEvent', 'IssuesEvent', 'PullRequestEvent', 'ForkEvent'])
+
+// {
+//   user,
+//   time,
+//   type,
+//   meta,
+// }
+
+console.log(myData);
+
 class App extends Component {
+  componentDidMount(){
+    d3.select("#svg")
+  .selectAll("circle")
+  .data(myData)
+  .enter().append("circle")
+  .attr('r', (d, i, l) => {
+      return 4;
+    }
+  )
+  .attr()
+  .attr('cx', (d, i, l) => {
+    return i;
+  })
+  .attr('cy', d => d);
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <svg id="svg">
+
+      </svg>
     );
   }
 }
