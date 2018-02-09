9245b0e8ae7d5a79b342ff317633389939be62f9
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-02-09 15:58:19 +0100

    Draw squares in grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,32 +7,40 @@ import data from './data.json';
 import * as d3 from 'd3';
 import * as scaleChromatic from 'd3-scale-chromatic';
 
-const myData = data.filter(d => !d.actor_login.includes('bot')).map(e => ({
-  user: e.actor_login,
-  type: e.type,
-  date: new Date(e.created_at),
-  metadata: e.payload,
-}));
-
-const eventsByUser = d3
+const igorEvents = d3
   .nest()
-  .key(d => d.user)
-  .object(myData);
+  .key(d => d.actor_login)
+  .key(d => {
+    var date = new Date(d.created_at);
+    return date.toDateString();
+  })
+  .entries(data)
+  .find(({ key }) => key === 'igor-dv').values;
+
+console.log(igorEvents);
+
+const minDate = d3.timeDay.floor(
+  new Date(Math.min(...data.map(({ created_at }) => new Date(created_at)))),
+);
+const maxDate = d3.timeDay.ceil(
+  new Date(Math.max(...data.map(({ created_at }) => new Date(created_at)))),
+);
+const dateRange = d3.timeDay.range(minDate, maxDate);
 
 const squareSize = 10;
 const gutter = 5;
-const xScaleSquare = x =>  x * (squareSize + gutter);
-
+const xScaleSquare = x => x * (squareSize + gutter);
+const eventTypes = [
+  'PushEvent',
+  'IssueCommentEvent',
+  'DeleteEvent',
+  'IssuesEvent',
+  'PullRequestEvent',
+  'ForkEvent',
+];
 const typeScale = d3
   .scaleOrdinal(scaleChromatic.schemeAccent)
-  .domain([
-    'PushEvent',
-    'IssueCommentEvent',
-    'DeleteEvent',
-    'IssuesEvent',
-    'PullRequestEvent',
-    'ForkEvent',
-  ]);
+  .domain(eventTypes);
 
 // {
 //   user,
@@ -43,16 +51,26 @@ const typeScale = d3
 
 class App extends Component {
   componentDidMount() {
-    d3
+    // console.log(eventsByUser);
+    const groupSelector = d3
       .select('#svg')
-      .selectAll('circle')
-      .data(eventsByUser.ndelangen)
+      .selectAll('rect')
+      .data(
+        [].concat(
+          ...igorEvents.map(({ values }, y) =>
+            values.map((event, x) => ({ ...event, x, y })),
+          ),
+        ),
+      )
       .enter()
-      .append('svg:rect')
+      .append('rect')
       .attr('width', squareSize)
       .attr('height', squareSize)
-      .attr('x', (d, i) => {
-        return xScaleSquare(i);
+      .attr('x', ({ x }) => {
+        return xScaleSquare(x);
+      })
+      .attr('y', ({ y }) => {
+        return xScaleSquare(y);
       })
       .attr('fill', ({ type }) => {
         return typeScale(type);
